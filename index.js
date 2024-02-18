745a7d6911c2761cb07c6595b402643536d9436b
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2024-02-18 21:58:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,74 @@
-let tablinks = document.getElementsByClassName('tab-links');
-let tabcontents = document.getElementsByClassName('tab-contents');
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-function opentab(tabname, event) {
-    for (let tablink of tablinks) {
-        tablink.classList.remove('active-link');
-    }
-    for (let tabcontent of tabcontents) {
-        tabcontent.classList.remove('active-tab');
-    }
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
 
-    event.currentTarget.classList.add('active-link');
-    document.getElementById(tabname).classList.add('active-tab');
-}
-var sidemenu = document.getElementById("sidemenu")
-function openmenu(){
-    sidemenu.style.right = "0";
-}
-function closemenu(){
-    sidemenu.style.right = '-200px';
-}
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbxJkyq9U9SQ1WG_LEsE3b8hErYh-j1ruyXpClgcu-RR6YBiYtkNlcUwk-vxvUCU1YAUvA/exec'
-  const form = document.forms['submit-to-google-sheet']
-  const msg = document.getElementById('msg')
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
 
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => {
-        msg.innerHTML='Message sent successfully'
-        setTimeout(function(){
-            msg.innerHTML=''
-        },5000)
-        form.reset();
-      })
-      .catch(error => console.error('Error!', error.message))
-  })
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Front-End Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings: ["Front-End Developer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
+});
